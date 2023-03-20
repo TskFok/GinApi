a575b2d8c4ab74611f0c0c584ad01f344180227a
ushopal <yi.yang@ushopal.com>
2023-03-20 14:26:38 +0800

    格式化日期

--- app/model/baseModel.go
+++ app/model/baseModel.go
@@ -1,10 +1,24 @@
 package model
 
 import (
+	"database/sql/driver"
+	"fmt"
 	"time"
 )
 
+type LocalTime time.Time
+
+func (t *LocalTime) MarshalJSON() ([]byte, error) {
+	tTime := time.Time(*t)
+
+	return []byte(fmt.Sprintf("\"%v\"", tTime.Format("2006-01-02 15:04:05"))), nil
+}
+
+func (t *LocalTime) Value() (driver.Value, error) {
+	return time.Time(*t), nil
+}
+
 type BaseModel struct {
-	CreatedAt time.Time `json:"created_at" gorm:"column:created_at;type:DATETIME;NOT NULL"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at;type:DATETIME;NOT NULL"`
+	CreatedAt *LocalTime `json:"created_at" gorm:"column:created_at;type:DATETIME;NOT NULL"`
+	UpdatedAt *LocalTime `json:"updated_at" gorm:"column:updated_at;type:DATETIME;NOT NULL"`
 }
